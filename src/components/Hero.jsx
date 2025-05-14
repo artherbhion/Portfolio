0c2b954a0ead1d527fd2b198e05fa3ca4f8a09bb
artherbhion <147584933+artherbhion@users.noreply.github.com>
2025-05-14 18:24:03 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -158,7 +158,7 @@ const profile = {
 
                 {/* Contact Button */}
                 <a
-                  href=""
+                  href="https://drive.google.com/file/d/1Zh7xx4SzBp9v27rh74cYQURfQp6Zhd53/view?usp=sharing"
                   className="group relative inline-flex items-center justify-center gap-3 bg-gradient-to-r from-blue-500 to-teal-400 p-0.5 rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]">
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-blue-500 group-hover:to-teal-400">
                     <span className="relative flex items-center justify-center gap-2 text-white font-medium">
